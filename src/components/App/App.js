28733c80688a9db8939012042ed56de5329cbcc5
Mateusz Rozenkiewicz <rodzyn22@gmail.com>
2021-01-03 13:52:56 +0100

    add List to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './App.scss';
+import List from '../List/List.js';
 
 const App = () => (
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
+        <List />
       </main>
     );
 
