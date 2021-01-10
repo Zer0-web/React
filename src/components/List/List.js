e407f0e2deceecde55ec83e3bab9ac56f6dda0dc
Mateusz Rozenkiewicz <rodzyn22@gmail.com>
2021-01-10 06:42:21 +0100

    add children to list.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+
 
 const List = props => (
-      <section className={styles.component}>
-        <Hero titleText={props.title} />
-      </section>
+  <section className={styles.component}>
+    <Hero titleText={props.title} />
+      <div className={styles.description}>
+        {props.children}
+      </div>
+  </section>
 );
 
 
 export default List;
+
